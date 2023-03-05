a41eba068aa519ec07f0b6746f6cd803c1eecbe9
Dennis Demir <dennisjdemir@gmail.com>
2023-03-05 11:46:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat(name) {
+    [cats.splice(4,0,'Ralph')]
+};
+
+function destructivelyPrependCat(name) {
+    [cats.splice(0,0,'Bob')]
+};
+
+function destructivelyRemoveLastCat() {
+    [cats.pop()]
+};
+
+function destructivelyRemoveFirstCat() {
+    [cats.shift()]
+};
+
+function appendCat(name) {
+    const newCats = [...cats,'Broom'];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newerCats = ['Arnold',...cats];
+    return newerCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats]
+    copyOfCats.pop()
+    return copyOfCats
+}
+
+function removeFirstCat() {
+    const catsAgain = [...cats];
+    catsAgain.shift();
+    return catsAgain;
+}
